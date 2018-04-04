7df0739f53e942b3c93cc6fa3fb9a2aa8914aa2e
zhang rui <905514896@qq.com>
2018-04-04 19:37:32 +0800

    twos

--- mod.js
+++ mod.js
@@ -1,23 +1,23 @@
 function postBo(obj){
     var Oparent=document.getElementById("conText")
-    	Oparent.innerHTML=obj; 
-    	litext=Oparent.getElementsByTagName("h2")[0].innerText;
-    	var Oul=document.getElementById("listul")
-    	var Oli=document.createElement("li");
-    		Oul.appendChild(Oli); 
-    	var num=Oul.getElementsByTagName("li").length;
-    		Oli.innerText=num+" "+litext;
-    	myStrage(num,obj)
+      Oparent.innerHTML=obj; 
+      litext=Oparent.getElementsByTagName("h2")[0].innerText;
+      var Oul=document.getElementById("listul")
+      var Oli=document.createElement("li");
+        Oul.appendChild(Oli); 
+      var num=Oul.getElementsByTagName("li").length;
+        Oli.innerText=num+" "+litext;
+      myStrage(num,obj)
 }
 function write(title,content){
-	return "<h2>"+Trim(title,"g")+"</h2>"+"<div>"+Trim(content,"g")+"</div>"
+  return "<h2>"+Trim(title,"g")+"</h2>"+"<div>"+Trim(content,"g")+"</div>"
 }
 //json对象增加数据
 var myjson={}
 function myStrage(index,value){
-	
-	myjson[index]=value
-	
+  
+  myjson[index]=value
+  
 }
 //去掉所有空格
 function Trim(str,is_global)
@@ -32,14 +32,14 @@ function Trim(str,is_global)
 }
 //点击加载
 function liclick(Oul,obj){
-	var lis=Oul.getElementsByTagName("li");
-	var condiv=document.getElementById("conText")
-		for (var i = 0 ; i<lis.length; i++){
-			lis[i].index=i
-			lis[i].onclick=function(){
-			condiv.innerHTML=myjson[this.index+1]	
-		}
-	}
+  var lis=Oul.getElementsByTagName("li");
+  var condiv=document.getElementById("conText")
+    for (var i = 0 ; i<lis.length; i++){
+      lis[i].index=i
+      lis[i].onclick=function(){
+      condiv.innerHTML=myjson[this.index+1] 
+    }
+  }
 }
 function stopBubble(e){
           if(e && e.stopPropagation) {//非IE 
@@ -55,11 +55,37 @@ function fourMove(){
   Four.onclick=function(e){
     stopBubble(e)
     shopbtn.style.transform="translateY(200px)";
+    Tostartcss(shopbtn,"transform","translateY(-120px)")
 }
+  
 }
- 
-function Tostartcss(){
+
+function twoMove(){
+  var Two=document.getElementById("two");
+  var Navlist=document.getElementById("Navlist");
+    Two.onclick=function(e){
+    stopBubble(e)
+    Navlist.style.transform="translateX(0px)";
+    Tostartcss(Navlist,"transform","translateX(-220px)")
+      }
+    Navlist.onmouseover=function(){
+      this.style.transform="translateX(0)";
+      this.style.boxShadow="none";
+    }
+    Navlist.onmouseout=function(){
+      this.style.transform="translateX(-220px)";
+      this.style.boxShadow="0 0 1px 1px #fff";
+    }
+
+   
+}
+
+
+function Tostartcss(obj,attr,target){
   window.onclick=function(){
-    shopbtn.style.transform="translateY(-120px)";
+    obj.style[attr]=target;
   }
 }
+
+
+
