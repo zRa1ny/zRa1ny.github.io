6799dd0f01c927c1acad1eba2efd49361484e53e
unknown <18271683093>
2019-10-03 03:16:14 +0800

    1,0,1

--- demo/zz/js/main.js
+++ demo/zz/js/main.js
@@ -4,7 +4,7 @@
 //第一时间执行，不能放置到windown.onload中
 setRem(28, document, window);
 window.isReady = true;
-setLoadAni(function() {
+setLoadAni(function () {
     if (window.isReady) {
         window.isReady = false;
         init({
@@ -13,40 +13,43 @@ setLoadAni(function() {
     }
 });
 
-init({
-    companyData: companyData
-});
+// init({
+//     companyData: companyData
+// });
 
 
-function init(obj) {
+function init (obj) {
     var $index = $(".index"),
         $container = $('.container'),
         ani = new AnimateControl(),
         companyData = obj.companyData,
         $jobPage = $('.job');
 
+
+    var carAni = new Car()
+
     $index.animate({
         top: "0",
         opacity: '1'
-    }, function() {
+    }, function () {
         ani.swiperAnimate('.index');
     })
 
-    $('.cir').click(function() {
+    $('.cir').click(function () {
         console.log('click')
         var pageName = $(this).attr('page');
         $('.' + pageName).addClass('animated zoomIn').show();
         ani.swiperAnimate('.' + pageName);
     })
 
-    function hidePage() {
+    function hidePage () {
         console.log(this)
         $(this).removeClass('animated scaleOut').hide();
         $(this).off('animationend', hidePage);
         $(this).off('webkitAnimationEnd', hidePage);
     }
 
-    $('.title-back').click(function() {
+    $('.title-back').click(function () {
         var $page = $(this).parents('.page');
         $page.removeClass('zoomIn').addClass('scaleOut');
         $page.on('webkitAnimationEnd', hidePage)
@@ -54,23 +57,23 @@ function init(obj) {
     })
 
 
-    $(".enter").click(function(e) {
+    $(".enter").click(function (e) {
         e.stopPropagation();
         // $container.fadeIn()
         $index.animate({
             top: "-100vh",
             opacity: '0'
-        }, function() {
+        }, function () {
             $container.animate({
                 top: "0",
                 opacity: '1'
-            }, function() {})
+            }, function () { })
 
         })
 
     })
 
-    $('.join-2-big-show').click(function() {
+    $('.join-2-big-show').click(function () {
         $('.join .small-join').css({
             "max-height": "0"
         });
@@ -79,7 +82,7 @@ function init(obj) {
         });
     })
 
-    $('.join-backpage').click(function() {
+    $('.join-backpage').click(function () {
         $('.join .small-join').css({
             "max-height": "100vh"
         });
@@ -88,7 +91,7 @@ function init(obj) {
         });
     })
 
-    $('.showActive').click(function() {
+    $('.showActive').click(function () {
         $('.depart .nomral-box').css({
             "max-height": "0"
         });
@@ -97,7 +100,7 @@ function init(obj) {
         });
     })
 
-    $('.depart-backpage').click(function() {
+    $('.depart-backpage').click(function () {
         $('.depart .nomral-box').css({
             "max-height": "100vh"
         });
@@ -107,27 +110,29 @@ function init(obj) {
     })
 
 
-    $('.group .campany').click(function(e) {
+    $('.group .campany').click(function (e) {
         var curData = companyData[$(this).attr('company')];
         $(this).addClass('active').siblings().removeClass('active');
+        $('#roadcar').removeClass('c1 c2 c3 c4 c5 c6 c7 c8 c9').addClass($(this).attr('company'));
         $('#group-deatail-title').text(curData.title)
         $('#group-deatail-img-src').attr('src', './images/company/' + curData.img)
         $('#c-name').text(curData.name)
         $('#c-text').text(curData.content)
         $('#group-deatail').fadeIn();
     })
-    $('.group-deatail-back').click(function() {
+    $('.group-deatail-back').click(function () {
         $('#group-deatail').fadeOut()
     })
 
-    $('.cliclApply').click(function(e) {
+    $('.cliclApply').click(function (e) {
+        console.log("show")
         $jobPage.show();
     })
-    $('.title-back-job').click(function() {
+    $('.title-back-job').click(function () {
         $jobPage.hide();
     })
 
-    $('.unit-name').click(function() {
+    $('.unit-name').click(function () {
         console.log($(this).index())
 
     })
